9404f1b623dc669983408332a0102824d59a8672
Blagoja Blazhevski <128937678+blagojablazhevski@users.noreply.github.com>
2025-09-14 13:55:20 +0200

    refactor(game): improved code readability
    Merged multiple case statements in Game component.

--- components/Dojo/Kana/Game/index.tsx
+++ components/Dojo/Kana/Game/index.tsx
@@ -1,45 +1,54 @@
-'use client';
-import clsx from 'clsx';
-import { useEffect } from 'react';
-import Return from '@/components/reusable/Game/ReturnFromGame';
-import Pick from './Pick';
-import Input from './Input';
-import useKanaKanjiStore from '@/store/useKanaKanjiStore';
-import useStatsStore from '@/store/useStatsStore';
-import Stats from '@/components/reusable/Game/Stats';
+"use client";
+import clsx from "clsx";
+import {useEffect, useMemo} from "react";
+import Return from "@/components/reusable/Game/ReturnFromGame";
+import Pick from "./Pick";
+import Input from "./Input";
+import useKanaKanjiStore from "@/store/useKanaKanjiStore";
+import useStatsStore from "@/store/useStatsStore";
+import Stats from "@/components/reusable/Game/Stats";
 
 const Game = () => {
-  const showStats = useStatsStore(state => state.showStats);
+	const showStats = useStatsStore((state) => state.showStats);
 
-  const resetStats = useStatsStore(state => state.resetStats);
+	const resetStats = useStatsStore((state) => state.resetStats);
 
-  const gameMode = useKanaKanjiStore(state => state.selectedGameModeKana);
+	const gameMode = useKanaKanjiStore((state) => state.selectedGameModeKana);
 
-  useEffect(() => {
-    resetStats();
-  }, []);
+	useEffect(() => {
+		resetStats();
+	}, []);
 
-  return (
-    <div
-      className={clsx(
-        'flex flex-col gap-6 md:gap-10 items-center min-h-[100dvh] max-w-[100dvw] px-4'
-        // "bg-[url('/wallpapers/neonretrocarcity.jpg')] bg-cover bg-center"
-        // "bg-[url('/wallpapers/kanaDojoWallpaper.png')] bg-cover bg-center backdrop-blur-lg"
-      )}
-    >
-      {showStats && <Stats />}
-      <Return isHidden={showStats} href='/kana' />
-      {gameMode.toLowerCase() === 'pick' ? (
-        <Pick isHidden={showStats} />
-      ) : gameMode.toLowerCase() === 'reverse-pick' ? (
-        <Pick isHidden={showStats} isReverse={true} />
-      ) : gameMode.toLowerCase() === 'input' ? (
-        <Input isHidden={showStats} />
-      ) : gameMode.toLowerCase() === 'reverse-input' ? (
-        <Input isHidden={showStats} isReverse={true} />
-      ) : null}
-    </div>
-  );
+	const renderPick = useMemo(() => {
+		const mode = gameMode.toLowerCase();
+
+		const isReverse = mode.includes("reverse");
+		const isPick = mode.includes("pick");
+		const Component = isPick ? Pick : Input;
+
+		return (
+			<Component
+				isReverse={isReverse}
+				isHidden={showStats}
+			/>
+		);
+	}, [gameMode, showStats]);
+
+	return (
+		<div
+			className={clsx(
+				"flex flex-col gap-6 md:gap-10 items-center min-h-[100dvh] max-w-[100dvw] px-4"
+				// "bg-[url('/wallpapers/neonretrocarcity.jpg')] bg-cover bg-center"
+				// "bg-[url('/wallpapers/kanaDojoWallpaper.png')] bg-cover bg-center backdrop-blur-lg"
+			)}>
+			{showStats && <Stats />}
+			<Return
+				isHidden={showStats}
+				href="/kana"
+			/>
+			{renderPick}
+		</div>
+	);
 };
 
 export default Game;
